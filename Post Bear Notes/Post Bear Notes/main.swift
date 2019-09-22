def607200ee9cb758929d5f4c9c5bcede604d87c
Tianyun Shan <nuynait@gmail.com>
2019-09-22 15:21:41 -0400

    Adds Post Bear Notes Swift Scripting Project
    + Create Xcode project for scripting.
    + Adds gitignore for Xcode swift.

---
+++ Post Bear Notes/Post Bear Notes/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Post Bear Notes
+//
+//  Created by Tianyun Shan on 2019-09-22.
+//  Copyright © 2019 Hourglass Lab. All rights reserved.
+//
+
+import Foundation
+
+if CommandLine.argc < 2 {
+    print("No arguments are passed. Expect file path for the exported post. Usually in ~/Downloads folder.")
+} else {
+    
+}
+
